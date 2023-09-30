9334f97414f7e3b33d73d38e2fef58e3ae067cd6
Thomasmp00 <thomaxx00@gmail.com>
2023-09-30 15:17:55 +0200

    Key encryption seperation

--- Encrypter/CoolEncrypter/Encryption.cs
+++ Encrypter/CoolEncrypter/Encryption.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 
@@ -6,60 +7,100 @@ namespace Encrypter;
 
 public class Encryption
 {
+    private Boolean appOn = true;
+    private Boolean messageIsnotEncrypted = true;
+    private String usedOption;
+    public byte[] nonce;
+    public byte[] ciphertext;
+    public byte[] tag;
+    
+    
     
     public void Main()
     {
-        Console.WriteLine("Enter the message you want to encrypt:");
-        string userMessage = Console.ReadLine();
-
-        EncryptMessage(userMessage);
         
-        ReadAndPrintNotSoSecretMessage();
+        while (appOn)
+        {
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("1: Encrypt a message");
+            Console.WriteLine("2: Decrypt a message");
+            Console.WriteLine("3: Reset and exit the app");
+            usedOption = Console.ReadLine();
+            cryptoOption();
+        }
+        
 
     }
 
-    public void EncryptMessage(string myString)
+    public void cryptoOption()
     {
-        var key = new byte[32];
-        RandomNumberGenerator.Fill(key);
+        if (usedOption.Equals("1"))
+        {
+            EncryptMessage();
+        }else if (usedOption.Equals("2"))
+        {
+            if (messageIsnotEncrypted = false)
+            {
+                DecryptionOption();
+            }
+            else
+            {
+                Console.WriteLine("You have to encrypt a message before you can decrypt one...");
+            }
+            
+        }else if (usedOption.Equals("3"))
+        {
+            appOn = false;
+        }
+        
+    }
 
-        var nonce = new byte[12];
+    public void EncryptMessage()
+    {
+        Console.WriteLine("Enter the message you want to encrypt:");
+        string myString = Console.ReadLine();
+        
+        // var key = new byte[32];
+        // RandomNumberGenerator.Fill(key);
+        Console.WriteLine("Write a 32 character long key for the encryption");
+        var keyText = Console.ReadLine();
+
+        Byte[] keyByte = Encoding.ASCII.GetBytes(keyText);
+
+        nonce = new byte[AesGcm.NonceByteSizes.MaxSize];
         RandomNumberGenerator.Fill(nonce);
 
-        var plaintextBytes = Encoding.UTF8.GetBytes(myString);
-        var ciphertext = new byte[plaintextBytes.Length];
-        var tag = new byte[AesGcm.TagByteSizes.MaxSize];
-
-        using (var aes = new AesGcm(key))
+        
+         var plaintextBytes = Encoding.UTF8.GetBytes(myString);
+         ciphertext = new byte[plaintextBytes.Length];
+         tag = new byte[AesGcm.TagByteSizes.MaxSize];
+        
+        if(keyText.Length == 32){
+        using (var aes = new AesGcm(keyByte))
         {
             aes.Encrypt(nonce, plaintextBytes, ciphertext, tag);
-        }
+            SecretMessage secretMessage = new SecretMessage
+            {
+                Nonce = nonce,
+                CipherText = ciphertext,
+                Tag = tag
+            };
 
-        SecretMessage secretMessage = new SecretMessage
+            string json = JsonSerializer.Serialize(secretMessage);
+            Console.WriteLine("Encrypted JSON:");
+            Console.WriteLine(json);
+            
+            string fileName = @"MySacredText.json";
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+            File.WriteAllText(filePath, json);
+            messageIsnotEncrypted = false;
+        }}
+        else
         {
-            Key = key,
-            Nonce = nonce,
-            CipherText = ciphertext,
-            Tag = tag
-        };
-
-        string json = JsonSerializer.Serialize(secretMessage);
-        Console.WriteLine("Encrypted JSON:");
-        Console.WriteLine(json);
-
-        string fileName = @"MySacredText.json";
-        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
-
-        File.WriteAllText(filePath, json);
-
+            Console.WriteLine("Key is not the right length");
+        }
         
-        string decryptedMessage = Decrypt(ciphertext, nonce, tag, key);
-
-        
-        var path = Path.Combine( "NotSoSecretMessage.json");
-        string filePath2 = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,  path);
-        
-        File.WriteAllText(filePath2, decryptedMessage);
     }
     static string Decrypt(byte[] ciphertext, byte[] nonce, byte[] tag, byte[] key)
     {
@@ -71,6 +112,22 @@ public class Encryption
 
             return Encoding.UTF8.GetString(plaintextBytes);
         }
+    }
+
+    public void DecryptionOption()
+    {
+        Console.WriteLine("Write the correct key to decrypt the message");
+        var decryptionKey = Console.ReadLine();
+        var decryptionKeyByte = Encoding.ASCII.GetBytes(decryptionKey);
+        
+        string decryptedMessage = Decrypt(ciphertext, nonce, tag, decryptionKeyByte);
+
+        
+        var path = Path.Combine( "NotSoSecretMessage.json");
+        string filePath2 = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,  path);
+        
+        File.WriteAllText(filePath2, decryptedMessage);
+        ReadAndPrintNotSoSecretMessage();
     }
     
     public void ReadAndPrintNotSoSecretMessage()
@@ -94,3 +151,4 @@ public class Encryption
     
     
 }
+
--- Encrypter/CoolEncrypter/obj/Debug/net8.0/CoolEncrypter.AssemblyInfo.cs
+++ Encrypter/CoolEncrypter/obj/Debug/net8.0/CoolEncrypter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoolEncrypter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e0e0885d389019a25790140288e7575b3c6ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoolEncrypter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoolEncrypter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
